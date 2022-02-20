d47010d0a961ab3917a4edc2b3c6feaf34321dfb
Rustanov <akbarrustanov@gmail.com>
2022-02-20 20:25:41 +0600

    admin add

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 from .models import Projects, Workers, Administrator, Supervisor, Tasks
+
+
+admin.site.register(Projects)
+admin.site.register(Workers)
+admin.site.register(Administrator)
+admin.site.register(Supervisor)
+admin.site.register(Tasks)
