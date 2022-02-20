e52f7e70fac81eb358524d80594529defd685e0c
Rustanov <akbarrustanov@gmail.com>
2022-02-20 18:27:41 +0600

    Models have been changed

--- main/models.py
+++ main/models.py
@@ -9,7 +9,15 @@ from django.db import models
 from django.db.models import Func, Q
 
 
+class Administrator(models.Model):
+    """ Руководитель проекта """
+
+    name = models.CharField(max_length=25)
+
+
 class Supervisor(models.Model):
+    """ Руководитель проекта """
+
     name = models.CharField(max_length=25)
 
     class Meta:
@@ -17,15 +25,9 @@ class Supervisor(models.Model):
         verbose_name_plural = 'Задачи'
 
 
-class Tasks(models.Model):
-    task = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name = 'Задача'
-        verbose_name_plural = 'Задачи'
-
-
 class Workers(models.Model):
+
+    """ Разработчики проекта """
     Role_choice = (
         ('F', 'Front-End'),
         ('B', 'Back-End'),
@@ -43,13 +45,26 @@ class TsTzRange(Func, ABC):
     output_field = DateTimeRangeField()
 
 
+class Tasks(models.Model):
+    """ Задачи проекта """
+
+    task = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+
 class Projects(models.Model):
+    """  Проект """
+
     title = models.CharField(max_length=25)
     status = models.BooleanField(default=False)
     start_date = models.DateField()
     end_date = models.DateField()
     supervisor = models.ForeignKey(Supervisor, on_delete=models.CASCADE)
-    workers = models.ForeignKey(Workers, on_delete=models.CASCADE)
+    workers = models.ManyToManyField(Workers, on_delete=models.CASCADE)
+    admin = models.OneToOneField(Administrator, on_delete=models.CASCADE)
     task = models.ForeignKey(Tasks, on_delete=models.CASCADE)
 
     class Meta:
